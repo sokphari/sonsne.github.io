1e916d2e6aa37cf8b290f41dbd2671ffe7042018
sokphari <sokphari8@gmail.com>
2025-05-05 20:08:17 +0700

    studentsys

---
+++ javascript/javascript.js
@@ -0,0 +1,320 @@
+
+const cambodiaProvincesWithDistricts = [
+        {
+            province: "Banteay Meanchey",
+            districts: [
+                "Mongkol Borei",
+                "Phnum Srok",
+                "Preah Netr Preah",
+                "Ou Chrov",
+                "Serei Saophoan (Krong)",
+                "Thma Puok",
+                "Svay Chek",
+                "Malai",
+                "Poipet (Krong)"
+            ]
+        },
+        {
+            province: "Battambang",
+            districts: [
+                "Banan",
+                "Thma Koul",
+                "Battambang (Krong)",
+                "Bavel",
+                "Aek Phnum",
+                "Moung Ruessei",
+                "Rotonak Mondol",
+                "Sangkae",
+                "Samlout",
+                "Sampov Lun",
+                "Phnum Proek",
+                "Kamrieng",
+                "Koas Krala",
+                "Rukhak Kiri"
+            ]
+        },
+        {
+            province: "Kampong Cham",
+            districts: [
+                "Batheay",
+                "Chamkar Leu",
+                "Cheung Prey",
+                "Dambae",
+                "Kampong Cham (Krong)",
+                "Kampong Siem",
+                "Kang Meas",
+                "Kaoh Soutin",
+                "Krouch Chhmar",
+                "Memot",
+                "Ou Reang Ov",
+                "Ponhea Kraek",
+                "Prey Chhor",
+                "Srei Santhor",
+                "Stueng Trang",
+                "Tboung Khmum",
+                "Suong (Krong)"
+            ]
+        },
+        {
+            province: "Kampong Chhnang",
+            districts: [
+                "Baribour",
+                "Chol Kiri",
+                "Kampong Chhnang (Krong)",
+                "Kampong Leaeng",
+                "Kampong Tralach",
+                "Rolea B'ier",
+                "Sameakki Mean Chey",
+                "Tuek Phos"
+            ]
+        },
+        {
+            province: "Kampong Speu",
+            districts: [
+                "Basedth",
+                "Chbar Mon (Krong)",
+                "Kong Pisei",
+                "Aoral",
+                "Odongk",
+                "Phnum Sruoch",
+                "Samraong Tong",
+                "Thpong"
+            ]
+        },
+        {
+            province: "Kampong Thom",
+            districts: [
+                "Baray",
+                "Kampong Svay",
+                "Stueng Saen (Krong)",
+                "Prasat Ballangk",
+                "Prasat Sambour",
+                "Sandan",
+                "Santuk",
+                "Stoung",
+                "Taing Kouk"
+            ]
+        },
+        {
+            province: "Kampot",
+            districts: [
+                "Angkor Chey",
+                "Banteay Meas",
+                "Chhuk",
+                "Chum Kiri",
+                "Dang Tong",
+                "Kampong Trach",
+                "Tuek Chhou",
+                "Kampot (Krong)",
+                "Bokor (Krong)"
+            ]
+        },
+        {
+            province: "Kandal",
+            districts: [
+                "Kandal Stueng",
+                "Kien Svay",
+                "Khsach Kandal",
+                "Kaoh Thum",
+                "Leuk Daek",
+                "Lvea Aem",
+                "Mukh Kampul",
+                "Angk Snuol",
+                "Ponhea Lueu",
+                "S'ang",
+                "Ta Khmau (Krong)"
+            ]
+        },
+        {
+            province: "Kep",
+            districts: [
+                "Damnak Chang'aeur",
+                "Kaeb (Krong)"
+            ]
+        },
+        {
+            province: "Koh Kong",
+            districts: [
+                "Botum Sakor",
+                "Kiri Sakor",
+                "Koh Kong (Krong) / Khemarak Phoumin (Krong)",
+                "Mondol Seima",
+                "Srae Ambel",
+                "Thma Bang",
+                "Kampong Seila (Krong)"
+            ]
+        },
+        {
+            province: "Kratie",
+            districts: [
+                "Chhloung",
+                "Kratie (Krong)",
+
+"Preaek Prasab",
+                "Sambour",
+                "Snuol",
+                "Chitr Borei"
+            ]
+        },
+        {
+            province: "Mondulkiri",
+            districts: [
+                "Kaev Seima",
+                "Kaoh Nheaek",
+                "Ou Reang",
+                "Pech Chenda",
+                "Saen Monourom (Krong)"
+            ]
+        },
+        {
+            province: "Oddar Meanchey",
+            districts: [
+                "Anlong Veaeng",
+                "Banteay Ampil",
+                "Chongkal",
+                "Samraong (Krong)",
+                "Trapeang Prasat"
+            ]
+        },
+        {
+            province: "Pailin",
+            districts: [
+                "Pailin (Krong)",
+                "Sala Krau"
+            ]
+        },
+        {
+            province: "Phnom Penh (Autonomous Municipality)",
+            districts: [
+                "Chamkar Mon (Khan)",
+                "Doun Penh (Khan)",
+                "Prampir Meakkakra (Khan)",
+                "Tuol Kouk (Khan)",
+                "Dangkao (Khan)",
+                "Mean Chey (Khan)",
+                "Ruessei Kaev (Khan)",
+                "Sen Sok (Khan)",
+                "Por Senchey (Khan)",
+                "Chbar Ampov (Khan)",
+                "Chroy Changvar (Khan)",
+                "Prek Pnov (Khan)",
+                "Nirouth (Khan)",
+                "Boeng Keng Kang (Khan)"
+            ]
+        },
+        {
+            province: "Preah Sihanouk",
+            districts: [
+                "Mittakpheap (Khan) / Sihanoukville (Krong)",
+                "Prey Nob",
+                "Stueng Hav",
+                "Kampong Seila"
+            ]
+        },
+        {
+            province: "Preah Vihear",
+            districts: [
+                "Chey Saen",
+                "Chhaeb",
+                "Choam Khsant",
+                "Kuleaen",
+                "Rovieng",
+                "Sangkum Thmei",
+                "Tbaeng Meanchey (Krong)",
+                "Preah Vihear (Krong)"
+            ]
+        },
+        {
+            province: "Pursat",
+            districts: [
+                "Bakan",
+                "Kandieng",
+                "Krakor",
+                "Phnum Kravanh",
+                "Pursat (Krong)",
+                "Veal Veaeng"
+            ]
+        },
+        {
+            province: "Ratanakiri",
+            districts: [
+                "Andoung Meas",
+                "Banlung (Krong)",
+                "Bar Kaev",
+                "Koun Mom",
+                "Lumphat",
+                "Ou Chum",
+                "Ou Ya Dav",
+                "Ta Veaeng",
+                "Veun Sai"
+            ]
+        },
+        {
+            province: "Siem Reap",
+            districts: [
+                "Angkor Chum",
+                "Angkor Thom",
+                "Banteay Srei",
+                "Chi Kraeng",
+                "Kralanh",
+                "Puok",
+                "Prasat Bakong",
+                "Siem Reap (Krong)",
+                "Soutr Nikom",
+                "Srei Snam",
+                "Svay Leu",
+                "Varin"
+            ]
+        },
+        {
+            province: "Stung Treng",
+            districts: [
+                "Sesan",
+                "Siem Bouk",
+                "Siem Pang",
+                "Stung Treng (Krong)",
+                "Thala Barivat"
+            ]
+        },
+        {
+            province: "Svay Rieng",
+            districts: [
+                "Chantrea",
+                "Kampong Rou",
+                "Rumduol",
+                "Romeas Haek",
+                "Svay Chrum",
+                "Svay Rieng (Krong)",
+                "Svay Teab",
+                "Bavet (Krong)"
+            ]
+        },
+        {
+            province: "Takeo",
+            districts: [
+                "Angkor Borei",
+                "Bati",
+                "Borei Cholsar",
+                "Kiri Vong",
+                "Kaoh Andaet",
+                "Prey Kabbas",
+                "Samraong",
+                "Doun Kaev (Krong)",
+                "Tram Kak",
+
+"Treang"
+            ]
+        },
+        {
+            province: "Tboung Khmum",
+            districts: [
+                "Dambae",
+                "Krouch Chhmar",
+                "Memot",
+                "Ponhea Kraek",
+                "Suong (Krong)",
+                "Tboung Khmum",
+                "Ou Reang Ov"
+            ]
+        }
+    ];
